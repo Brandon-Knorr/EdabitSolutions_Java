c43b71ced500a5fb8da04b4154efbd6630f6f4ef
Brandon-Knorr <bknorr@my.wctc.edu>
2024-09-28 17:50:30 -0500

    [1.02] Inches to feet

--- src/edu/wctc/Main.java
+++ src/edu/wctc/Main.java
@@ -67,4 +67,15 @@ public class Main {
     public static boolean isEqual(int num1, int num2) {
         return num1 == num2;
     }
+
+
+
+    /*
+    Inches to Feet
+    - Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet.
+     */
+
+    public static int convert(int inches) {
+        return inches * 12;
+    }
 }
